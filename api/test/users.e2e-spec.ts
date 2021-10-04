fd3af7b453cb0d53f8a767cbbb3773e407d3024b
dgabka <gabka.daw@gmail.com>
2021-10-05 01:03:11 +0200

    add users e2e test

--- api/test/users.e2e-spec.ts
+++ api/test/users.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Connection } from 'mongoose';
 import * as request from 'supertest';
@@ -16,6 +16,11 @@ describe('UsersController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+      }),
+    );
     await app.init();
     dbConnection = moduleFixture
       .get<DatabaseService>(DatabaseService)
@@ -48,6 +53,160 @@ describe('UsersController (e2e)', () => {
       expect(user).toMatchObject({
         ...stub,
         password: expect.any(String),
+      });
+    });
+
+    it('should return 409 Conflict if user with given email already exists', async () => {
+      const stub = userStub();
+      await dbConnection.collection('users').insertOne(userStub());
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send(stub);
+
+      expect(response.status).toEqual(409);
+      expect(response.body).toMatchObject({
+        message: 'Email address must be unique',
+      });
+    });
+
+    it('should return 400 Bad Request if firstName is empty', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({ ...userStub(), firstName: '' });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: ['firstName must be longer than or equal to 1 characters'],
+      });
+    });
+
+    it('should return 400 Bad Request if firstName is longer than 32 characters', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({
+          ...userStub(),
+          firstName: Array.from({ length: 33 }).fill('a').join(),
+        });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: ['firstName must be shorter than or equal to 32 characters'],
+      });
+    });
+
+    it('should return 400 Bad Request if lastName is empty', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({ ...userStub(), lastName: '' });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: ['lastName must be longer than or equal to 1 characters'],
+      });
+    });
+
+    it('should return 400 Bad Request if lastName is longer than 32 characters', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({
+          ...userStub(),
+          lastName: Array.from({ length: 33 }).fill('a').join(),
+        });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: ['lastName must be shorter than or equal to 32 characters'],
+      });
+    });
+
+    it('should return 400 Bad Request if email address is not valid', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({
+          ...userStub(),
+          email: 'invalidEmail',
+        });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: ['email must be an email'],
+      });
+    });
+
+    it('should return 400 Bad Request if email address is missing', async () => {
+      const stub = userStub();
+      delete stub.email;
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send(stub);
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: expect.arrayContaining(['email should not be empty']),
+      });
+    });
+
+    it('should return 400 Bad Request if password is too short', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({
+          ...userStub(),
+          password: 'Test1',
+        });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: expect.arrayContaining([
+          'password must be longer than or equal to 8 characters',
+        ]),
+      });
+    });
+
+    it('should return 400 Bad Request if password does not contain an uppercase letter', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({
+          ...userStub(),
+          password: 'test@123',
+        });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: expect.arrayContaining([
+          'password must match /^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/ regular expression',
+        ]),
+      });
+    });
+
+    it('should return 400 Bad Request if password does not contain a lowecase letter', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({
+          ...userStub(),
+          password: 'TEST@123',
+        });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: expect.arrayContaining([
+          'password must match /^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/ regular expression',
+        ]),
+      });
+    });
+
+    it('should return 400 Bad Request if password does not contain a number', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send({
+          ...userStub(),
+          password: 'test@TEST',
+        });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toMatchObject({
+        message: expect.arrayContaining([
+          'password must match /^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/ regular expression',
+        ]),
       });
     });
   });
